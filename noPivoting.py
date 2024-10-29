6106d46b375fa935be38cb0214dbb0b1abbe21f3
Abhishek Mukherjee <abhishek.sc24m001@pg.iist.ac.in>
2024-10-29 16:22:29 +0530

    Added No Pivot Algorithm

---
+++ noPivoting.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def noPivot(matrix: np.ndarray):
+    matrix = matrix.astype(np.float64)
+    [m, _] = matrix.shape
+
+    for i in range(0, m):
+        for j in range(i+1, m):
+            matrix[j] -= (matrix[j][i]/matrix[i][i]) * matrix[i]
+
+    return matrix
